f5413ce1540baeb772a171016502abd81001123f
David Martinez <anddagger@gmail.com>
2022-10-24 23:54:22 -0500

    practiced syntax & structure; essential features of csharp

---
+++ csharp-players-guide/Level3 - Hello World/Program.cs
@@ -0,0 +1,19 @@
+﻿/*** Beyond Hello World ***/
+
+/** Variables **/
+string name;
+name = "Marti";
+Console.WriteLine("Hello, " + name + "!");
+
+
+/** Reading Text from The Console **/
+string firstName;
+Console.WriteLine("What is your first name?");
+firstName = Console.ReadLine();
+Console.WriteLine("Hi, " + firstName + "!");
+
+/* Challenge */
+Console.WriteLine("Bread is ready.");
+Console.WriteLine("Who is the bread for?");
+string recipient = Console.ReadLine();
+Console.WriteLine(recipient + " got bread.");
