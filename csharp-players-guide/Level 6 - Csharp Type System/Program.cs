039b4f5972e832eef6e565cf045c43d941943d49
David Martinez <anddagger@gmail.com>
2022-10-24 23:55:43 -0500

    practiced integer types; chars & strings; floating-point types; the bool type; type inference; the convert class and parse methods

---
+++ csharp-players-guide/Level 6 - Csharp Type System/Program.cs
@@ -0,0 +1,41 @@
+﻿/*** INTEGER TYPES ***/
+
+/* There are 8 integer types for storing ints of differing sizes and ranges: 
+ 
+ * int
+ * short
+ * long
+ * byte
+ * sbyte
+ * uint
+ * ushort
+ * ulong
+ 
+ */
+
+/** Declaring and Using Variables with Integer Types **/
+byte aSingleByte = 34;
+aSingleByte = 17;
+
+short aNum = 5039;
+aNum = -4354;
+
+long aVeryBigNum = 395_904_282_569;
+aVeryBigNum = -395_904_282_569;
+
+/** Binary and Hexadecimal Literals **/
+int thirteen = 0b1101;  // leading zeros are not required
+Console.WriteLine(thirteen);
+
+int theColorMagenta = 0xFF00FF;
+Console.WriteLine(theColorMagenta);
+
+
+/*** TEXT: CHARACTERS AND STRINGS ***/
+char aChar = 'a';
+Console.WriteLine(aChar);
+
+char aLetter = '\u0061';
+Console.WriteLine(aLetter);
+
+string msg = "Hello, world!";
